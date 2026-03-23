f2eccde61d6cd7dcdd483b4cf02e1dd8392dd106
ahnok0603 <nguyenthianhngoc060305@gmail.com>
2026-03-23 12:15:48 +0700

    change environment

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'http://localhost:4000' // TODO: Replace with your actual backend URL after deployment
+  apiUrl: 'https://sneaker-final-deploy.onrender.com' 
 };
